1f102231b129fa48e71030c99e8351a31e682013
pranay.mhaiske <pranay.mhaiske@kongsbergdigital.com>
2023-09-06 16:02:42 +0530

    Location_Changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace PageView_Capstone.Controllers
             return View();
         }
 
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
